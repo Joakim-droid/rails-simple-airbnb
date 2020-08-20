ee507602c7ca2adecf408db38c3eedc7bb543a68
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-08-20 20:47:47 +0200

    simple airbnb

---
+++ app/models/flat.rb
@@ -0,0 +1,40 @@
+class Flat < ApplicationRecord
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'House',
+  address: 'not in your neighbourhood',
+  description: 'interesting description',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Rooftop flat',
+  address: 'on the roof',
+  description: 'makes you feel like you are on top of the world',
+  price_per_night: 50,
+  number_of_guests: 1
+)
